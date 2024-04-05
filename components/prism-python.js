762475f494315a3c0d87f6c66b65489478b61c54
Michael Totaro <mtotaro@ncsu.edu>
2024-04-04 22:01:05 -0400

    Python and Swift

--- components/prism-python.js
+++ components/prism-python.js
@@ -12,7 +12,7 @@ Prism.languages.python = {
         alias: 'red',
     },
     'always-red': {
-        pattern: /(?<=\{)[\w.]+(?=\})|(?<=[\w\).])\}(?=\s|[^{]*$)|\{(?=[\w.])/,
+        pattern: /\{[\w.]+\}/,
         alias: 'red'
     },       
     'teal-highlight': {
