994510f06642e5809c56319dd46420896311c178
Michael Totaro <mtotaro@ncsu.edu>
2024-04-25 21:41:44 -0400

    Style

--- components/prism-javascript.js
+++ components/prism-javascript.js
@@ -74,7 +74,7 @@ Prism.languages.javascript = Prism.languages.extend('clike', {
 });
 
 Prism.languages.javascript['punctuation'] = /[{}[\];(),:]/
-Prism.languages.javascript['operator'] = /[<>]=?|[!=]=?=?|\+\+?|&&?|\|\|?|[?*/~^%]/,
+Prism.languages.javascript['operator'] = /[!=]=?=?|&&?|\|\|?|[?*/~^%]/,
 
 Prism.languages.javascript['class-name'][0].pattern = /(\b(?:class|extends|implements|instanceof|interface|new)\s+)[\w.\\]+/;
 
