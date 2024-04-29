6e90337cce85c0da571c5b4bb24cf0301004f440
Michael Totaro <mtotaro@ncsu.edu>
2024-04-29 13:00:21 -0400

    Style

--- components/prism-javafx.js
+++ components/prism-javafx.js
@@ -1,25 +1,27 @@
 Prism.languages.fxml = {
-
-    "fxml-red": {
-        pattern: /VBox|Label|Button|VBox\.margin|Insets|xml|import/,
-		lookbehind: true,
-		alias: 'white',
-		greedy: true
-    },
-
-
     "fxml-orange": {
         pattern: /version|encoding|alignment|spacing|prefWidth|prefHeight|xmlns|xmlns:fx|fx:controller|fx:id|text|style|onAction|bottom/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
     },
-
+    "fxml-red": {
+        pattern: /VBox|Label|Button|VBox\.margin|Insets|xml|import/,
+		lookbehind: true,
+		alias: 'white',
+		greedy: true
+    },
     "fxml-gray": {
         pattern: /(<\? \?>|=|\<|\>|\/>)|javafx\.geometry\.Insets|javafx\.scene\.control\.*|javafx\.scene\.layout\.*/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
+    },
+    "fxml-green": {
+        pattern: /"^.*"$/,
+		lookbehind: true,
+		alias: 'white',
+		greedy: true
     }
-    
+
 }
