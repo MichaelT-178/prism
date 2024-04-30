4e3825f4505ad31f2b1799a8586ab3ba75544a37
Michael Totaro <mtotaro@ncsu.edu>
2024-04-30 09:45:02 -0400

    Style

--- components/prism-javascript.js
+++ components/prism-javascript.js
@@ -1,6 +1,6 @@
 Prism.languages.javascript = Prism.languages.extend('clike', {
 	'yellow-js': {
-		pattern: /(?<=<)RouterBtn|Router(?=>)|Routes(?=>)|(?<=<)Route|(?<=<)HomePage|(?<=<)AppRouter|(?<=<)SecondPage|(?<=<)\bLink\b(?!Btn)|(?<=\/)Link(?!Btn)/,
+		pattern: /(?<=<)RouterBtn|Router(?=>)|Router(?= basename)|Routes(?=>)|(?<=<)Route|(?<=<)HomePage|(?<=<)AppRouter|(?<=<)SecondPage|(?<=<)\bLink\b(?!Btn)|(?<=\/)Link(?!Btn)/,
 		lookbehind: true,
 		alias: 'red',
 		greedy: true
