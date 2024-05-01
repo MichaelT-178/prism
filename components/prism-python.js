f935bf3b8b55eb482d4222a2876990da1e2de320
Michael Totaro <mtotaro@ncsu.edu>
2024-05-01 17:16:23 -0400

    Style

--- components/prism-python.js
+++ components/prism-python.js
@@ -8,7 +8,7 @@ Prism.languages.python = {
         alias: 'orange'
     },
     'self-red': {
-        pattern: /(?<=self\.)\w+\b|(?<=\.)\w+\b|self(?=\.)|\{(?=self|new_car|car_id|len|key|value|'Michael')|(?<=id|company|year|color|miles|key|DELETE|value|'Michael'|len\(car_list\))\}/,
+        pattern: /(?<=\.)\w+\b|self(?=\.)|\{(?=self|new_car|car_id|len|key|value|'Michael')|(?<=id|company|year|color|miles|key|value|'Michael'|len\(car_list\))\}|(?<=self\.)\w+\b/,
         alias: 'red',
     },
     'teal-highlight': {
