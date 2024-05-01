478b132e2ad9ee09a339c7aed29619e3acb7d965
Michael Totaro <mtotaro@ncsu.edu>
2024-05-01 16:06:00 -0400

    Stylr

--- components/prism-fxml.js
+++ components/prism-fxml.js
@@ -1,24 +1,30 @@
 Prism.languages.fxml = {
     "fxml-orange": {
-        pattern: /version|encoding|alignment|spacing|prefWidth|prefHeight|xmlns:fx|xmlns|fx:controller|fx:id|text|style|onAction|bottom/,
+        pattern: /version|encoding|alignment|spacing|prefWidth|prefHeight|xmlns:fx|xmlns|fx:controller|fx:id|text|style|onAction|bottom|2005/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
     },
     "fxml-red": {
-        pattern: /VBox.margin|VBox|Label|Button|Insets|xml|import/,
+        pattern: /VBox.margin|VBox|Label|Button|Insets|xml|import|Maker|Year|Cars/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
     },
     "fxml-gray": {
-        pattern: /(<\? \?>|=|\<|\>|\/>)|javafx\.geometry\.Insets|javafx\.scene\.control\.*|javafx\.scene\.layout\.*/,
+        pattern: /(<\? \?>|=|\<|\>|\/>)|javafx\.geometry\.Insets|javafx\.scene\.control\.*|javafx\.scene\.layout\.*|>(?= 2005)|,(?= Year)|(?<=2005);/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
     },
     "fxml-green": {
         pattern: /".*?"/,
+		lookbehind: true,
+		alias: 'white',
+		greedy: true
+    },
+    'fxml-purple': {
+        pattern: /SELECT|DISTINCT|FROM|WHERE/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
