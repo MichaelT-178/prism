8a7af7c4dc149e573f3700d249fb8376c1da1254
Michael Totaro <mtotaro@ncsu.edu>
2024-05-01 16:06:38 -0400

    Style

--- components/prism-fxml.js
+++ components/prism-fxml.js
@@ -1,6 +1,6 @@
 Prism.languages.fxml = {
     "fxml-orange": {
-        pattern: /version|encoding|alignment|spacing|prefWidth|prefHeight|xmlns:fx|xmlns|fx:controller|fx:id|text|style|onAction|bottom|2005/,
+        pattern: /version|encoding|alignment|spacing|prefWidth|prefHeight|xmlns:fx|xmlns|fx:controller|fx:id|text|style|onAction|bottom|2005|/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
