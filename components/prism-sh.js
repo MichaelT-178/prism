cd578526a0300846f4937403d370cd5d9af569ba
Michael Totaro <mstotaro@gmail.com>
2024-06-06 11:19:32 -0400

    Sh update

--- components/prism-sh.js
+++ components/prism-sh.js
@@ -2,43 +2,36 @@ Prism.languages.sh = {
     "sh-green": {
         pattern: /".*?"|'.*?'/,
 		lookbehind: true,
-		alias: 'white',
 		greedy: true
     },
     "sh-blue": {
         pattern: /mvn|stop_spring_boot|sleep/,
 		lookbehind: true,
-		alias: 'white',
 		greedy: true
     },
     "sh-red": {
-        pattern: /GREENISH|BLUE|NC|\$SPRING_BOOT_PID|\$\!|/,
+        pattern: /GREENISH|BLUE|NC|SPRING_BOOT_PID|\$\!/,
 		lookbehind: true,
-		alias: 'white',
 		greedy: true  
     },
     "sh-orange": {
         pattern: /0\.1|--quiet/,
 		lookbehind: true,
-		alias: 'white',
 		greedy: true  
     },
     "sh-teal": {
         pattern: /echo|kill|trap/,
 		lookbehind: true,
-		alias: 'white',
 		greedy: true  
     },
     "sh-purple": {
         pattern: /=/,
 		lookbehind: true,
-		alias: 'white',
 		greedy: true  
     },
     "comment": {
         pattern: /(^|[^\\])#.*/,
 		lookbehind: true,
-		alias: 'white',
 		greedy: true
     }
 }
