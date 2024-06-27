fff4121568cc8a74ed4cd052cbbcc1fd0364c1e2
Michael Totaro <mstotaro@gmail.com>
2024-06-26 21:42:43 -0400

    Swift

--- components/prism-customswift.js
+++ components/prism-customswift.js
@@ -41,7 +41,7 @@ Prism.languages.customswift = {
         greedy: true 
     },
     "cust-swift-light-green": {
-        pattern: /\bEditDestinationView|\bDestinationListingView|\bDestination|\bContentView|(?<=\[)Sight(?=\])/,
+        pattern: /\bEditDestinationView|\bDestinationListingView|\bDestination(?!])|\bContentView|(?<=\[)Sight(?=\])/,
         lookbehind: true,
         alias: 'light-green',
         greedy: true 
