156c43546e3a2895eed9f46f24f86347fa7537bd
Michael Totaro <mstotaro@gmail.com>
2024-09-29 17:35:31 -0400

    comment

--- components/prism-customproperties.js
+++ components/prism-customproperties.js
@@ -1,16 +1,16 @@
 Prism.languages.customproperties = {
     "prop-orange": {
-        pattern: /^\s*[^=\s]+/m,
+        pattern: /^\s*[^=#\s]+/m,
         lookbehind: true,
         alias: 'orange',
         greedy: true,
     },
-	"prop-green": {
-		pattern: /(?<==).*/,
-		lookbehind: true,
-		alias: 'green',
-		greedy: true
-	},
+    "prop-green": {
+        pattern: /(?<==)[^#\n]*/,
+        lookbehind: true,
+        alias: 'green',
+        greedy: true,
+    },
     'prop-comment': {
 		pattern: /(^|[^\\])#.*/,
 		lookbehind: true,
