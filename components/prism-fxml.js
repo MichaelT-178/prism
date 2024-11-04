f8b8fb375112d0813284374445a5d04fe24f6d4c
Michael Totaro <mstotaro@gmail.com>
2024-11-04 00:12:31 -0500

    Comment

--- components/prism-fxml.js
+++ components/prism-fxml.js
@@ -12,7 +12,7 @@ Prism.languages.fxml = {
 		greedy: true
     },
     "fxml-gray": {
-        pattern: /(<\? \?>|=|\<|\>|\/>)|javafx\.geometry\.Insets|javafx\.scene\.control\.*|javafx\.scene\.layout\.*|>(?= 2005)|,(?= Year)|(?<=2005);|Maker|Year|Cars|# DB_ENV can be default, test, or production/,
+        pattern: /(<\? \?>|=|\<|\>|\/>)|javafx\.geometry\.Insets|javafx\.scene\.control\.*|javafx\.scene\.layout\.*|>(?= 2005)|,(?= Year)|(?<=2005);|Maker|Year|Cars/,
 		lookbehind: true,
 		alias: 'white',
 		greedy: true
@@ -32,6 +32,12 @@ Prism.languages.fxml = {
     'fxml-teal': {
         pattern: /E06C75|ABB2BF|98C379|D19A66/,
 		lookbehind: true,
+		alias: 'white',
+		greedy: true
+    },
+    'fxml-comment': {
+        pattern: /# DB_ENV can be default, test, or production/,
+        lookbehind: true,
 		alias: 'white',
 		greedy: true
     }
