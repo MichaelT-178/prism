495e8cc6006fb3a6fc5ee78692eb49cdd2f1c1d2
Michael Totaro <mstotaro@gmail.com>
2025-02-11 22:22:58 -0500

    sCustomsql;

--- components/prism-customsql.js
+++ components/prism-customsql.js
@@ -1,6 +1,6 @@
 Prism.languages.customsql = {
 	"sql-purple": {
-		pattern: /\b(?!SCHEMA\b|PUBLIC\b|INFORMATION_SCHEMA|USERS\b)[A-Z]+\b/,
+		pattern: /\b(?!SCHEMA\b|PUBLIC\b|INFORMATION_SCHEMA|USERS|use\b)[A-Z]+\b/,
 		lookbehind: true,
 		alias: 'red',
 		greedy: true
